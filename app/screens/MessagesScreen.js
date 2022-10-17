b94cf489d9e30779bb85d356d0121b377456587c
NerVod <benjamin.jeannerot@gmail.com>
2022-10-17 13:09:06 +0200

    ListItem separator component

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import ListItem from '../components/ListItem';
+import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen';
 
 
@@ -30,6 +31,7 @@ function MessagesScreen(props) {
                                         subTitle={item.subTitile}
                                         image={item.image}
                                         />}
+                ItemSeparatorComponent={ ListItemSeparator }
             />
         </Screen>
     );
